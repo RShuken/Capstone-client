b5b3f0f3eab7e3adc0c51d7ed51ea44a86531dce
rshuken <ryanshuken@gmail.com>
2021-01-07 13:05:49 -0700

    more deploy issues

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  API_ENDPOINT: 'https://afternoon-waters-35759.herokuapp.com',
+  API_ENDPOINT:
+    'https://cors-anywhere.herokuapp.com/https://afternoon-waters-35759.herokuapp.com',
 };
