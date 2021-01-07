423460834cd9dababfdd5ca16c7073686e19f4f2
rshuken <ryanshuken@gmail.com>
2021-01-07 13:07:57 -0700

    1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  API_ENDPOINT:
-    'https://cors-anywhere.herokuapp.com/https://afternoon-waters-35759.herokuapp.com',
+  API_ENDPOINT: 'https://afternoon-waters-35759.herokuapp.com',
 };
